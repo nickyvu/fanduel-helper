79417eabe84e4c6eead3538cda50f93d6f91654f
Nick Nguyen <nickhxnguyen@gmail.com>
2014-02-09 10:51:51 -0600

    user can hover over names in entries lightbox for larger leagues

--- myscript.js
+++ myscript.js
@@ -1,11 +1,19 @@
-console.log("Test!");
-$(document).on("mouseenter", "div.user a", function() {
+$(document).on("mouseenter", "tr.lobbyitem div.user a", function() {
     var url = $(this).attr('href');
     $(this).parent().parent().append("<div class='results'></div>");
     $(".results").load("https://www.fanduel.com" + url+ " table");
 });
 
-$(document).on("mouseleave", "div.user a", function() {
+$(document).on("mouseleave", "tr.lobbyitem div.user a", function() {
   $(".results").remove();
 });
 
+$(document).on("mouseenter", "li a.username", function() {
+    var url = $(this).attr('href');
+    $(this).append("<div class='results'></div>");
+    $(".results").load(url+ " table");
+});
+
+$(document).on("mouseleave", "li a.username", function() {
+  $(".results").remove();
+});
